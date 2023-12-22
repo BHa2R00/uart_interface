7ec8889f9f99cc80dbcb07d6638c0b453639cb80
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2023-12-22 13:27:51 +0800

    Update uart_tb.sv

--- tb/uart_tb.sv
+++ tb/uart_tb.sv
@@ -1,55 +1,95 @@
+`include "../rtl/uart.v"
 `timescale 1ns/100ps
-`include "../rtl/uart_tx.v"
+
 module uart_tb;
 
+reg clk;
+initial clk = 0;
+always #8.9 clk = ~clk;
 reg rstn, enable;
 
-reg clk;
-initial clk = 1'b0;
-always #4.5 clk = ~clk;
+reg [31:0] div;
 
-wire u1_uart_tx_empty;
-reg u1_uart_tx_fill;
-wire u1_uart_tx_tx;
-reg [7:0] u1_uart_tx_tx_data;
-uart_tx u1_uart_tx(
-	.empty(u1_uart_tx_empty), 
-	.fill(u1_uart_tx_fill), 
-	.tx(u1_uart_tx_tx), 
-	.tx_data(u1_uart_tx_tx_data), 
-	.parity(2'b00), 
-	.div(32'd48), 
+wire tx_ack;
+reg tx_req;
+wire tx;
+reg [7:0] tx_data;
+uart_tx u_uart_tx(
+	.ack(tx_ack), 
+	.req(tx_req), 
+	.tx(tx), 
+	.tx_data(tx_data), 
+	.div(div), 
 	.enable(enable), 
 	.rstn(rstn), .clk(clk)
 );
 
-initial begin
-	rstn = 1'b0;
-	enable = 1'b0;
-	u1_uart_tx_fill = 1'b0;
-	u1_uart_tx_tx_data = 8'd0;
-	repeat(2) @(negedge clk);
-	rstn = 1'b1;
-	repeat(2) @(negedge clk);
-	enable = 1'b1;
-	repeat(2) @(negedge clk);
-	u1_uart_tx_fill = 1'b1;
-	repeat(100) begin
-		@(negedge u1_uart_tx_empty);
-		u1_uart_tx_fill = 1'b0;
-		u1_uart_tx_tx_data = $urandom_range(33, 126);
-		$display("u1_uart_tx_tx_data = %x", u1_uart_tx_tx_data);
-		@(posedge u1_uart_tx_empty);
-		u1_uart_tx_fill = 1'b1;
+wire rx_ack;
+reg rx_req;
+wire rx = tx;
+wire [7:0] rx_data;
+uart_rx u_uart_rx(
+	.ack(rx_ack), 
+	.req(rx_req), 
+	.rx(rx), 
+	.rx_data(rx_data), 
+	.div(div), 
+	.enable(enable), 
+	.rstn(rstn), .clk(clk)
+);
+
+task reset();
+	tx_req = 0;
+	rx_req = 0;
+	tx_data = 0;
+	div = $urandom_range(10,200);
+	rstn = 0;
+	repeat(5) @(posedge clk);
+	rstn = 1;
+endtask
+
+task set();
+	rstn = 1;
+	repeat(5) @(posedge clk);
+	rstn = 0;
+endtask
+
+integer fin;
+
+task test();
+	$display("start test");
+	fin = $fopen("../src/rc4.fth","r");
+	enable = 0;
+	repeat(5) @(posedge clk);
+	enable = 1;
+	repeat(5) @(posedge clk);
+	while(!$feof(fin)) begin
+		div = $urandom_range(10,200);
+		rx_req = ~rx_req;
+		repeat(5) @(posedge clk);
+		tx_data = $fgetc(fin);
+		tx_req = ~tx_req;
+		//@(negedge tx_ack or posedge tx_ack);
+		@(negedge rx_ack or posedge rx_ack);
+		$write("%c", rx_data);
+		repeat(1000) @(posedge clk);
 	end
-	repeat(2) @(negedge clk);
-	rstn = 1'b0;
-	repeat(2) @(negedge clk);
+	repeat(5) @(posedge clk);
+	enable = 0;
+	$fclose(fin);
+	$display("end test");
+endtask
+
+initial begin
+	reset();
+	test();
+	set();
 	$finish;
 end
+
 initial begin
 	$dumpfile("../work/uart_tb.fst");
-	$dumpvars(0,uart1_tb);
+	$dumpvars(0, uart_tb);
 end
 
 endmodule
